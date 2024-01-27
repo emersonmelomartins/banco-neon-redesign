31e9aa3917378fffdfb0cd055deca9c8761b434d
Emerson Melo Martins <emerson25xd@gmail.com>
2024-01-26 23:32:38 -0300

    section depoimentos

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var swiper = new Swiper(".slide-depoimentos", {
+  slidesPerView: 3,
+  spaceBetween: 32,
+  pagination: {
+    el: ".s-depoimentos .top .swiper-pagination",
+    clickable: true,
+  },
+});
