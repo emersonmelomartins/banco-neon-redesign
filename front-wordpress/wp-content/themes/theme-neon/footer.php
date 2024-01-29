e7f826dc1373eea58aac2ed7701c98a8d04e3a55
Emerson Melo Martins <emerson25xd@gmail.com>
2024-01-28 22:46:39 -0300

    wp_head e wp_footer

--- front-wordpress/wp-content/themes/theme-neon/footer.php
+++ front-wordpress/wp-content/themes/theme-neon/footer.php
@@ -176,5 +176,7 @@
     <script src="<?php echo get_template_directory_uri() ?>/js/swiper-bundle.min.js"></script>
     <script src="<?php echo get_template_directory_uri() ?>/js/aos.js"></script>
     <script src="<?php echo get_template_directory_uri() ?>/js/main.js"></script>
+
+    <?php wp_footer(); ?>
   </body>
 </html>
--- front-wordpress/wp-content/themes/theme-neon/header.php
+++ front-wordpress/wp-content/themes/theme-neon/header.php
@@ -70,6 +70,8 @@
       href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&family=Poppins:wght@400;500;600;700&display=swap"
       rel="stylesheet"
     />
+
+    <?php wp_head(); ?>
   </head>
 
   <body>
